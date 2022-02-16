768186301d97a695b74fd37741670f56d2aa42d0
Junghun Park <junghun@MacBook-Pro.local>
2022-02-16 15:50:30 +0900

    simple version with img

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -5,4 +5,53 @@
 //  Created by Junghun Park on 2022/02/16.
 //
 
-import Foundation
+import UIKit
+
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 1600000, 80000000, 77000000, 120000000]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bountyList.count //셀 몇개?
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //cell의 기본 타입은 UITableView인데 상속한 ListCell로 커스텀 하기 위해서 upper casting 필요, 캐스팅 할 때 옵셔널 이기 때문에 guard나 if 필요
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountLabel.text = "$ \(bountyList[indexPath.row])"
+        
+        return cell
+        
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
+//            return cell
+//        }else {
+//            return UITableViewCell()
+//        } ---? guard 랑 같은 의미
+    }
+    //UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("---> \(indexPath.row)")
+    }
+
+   
+
+}
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountLabel: UILabel!
+}
