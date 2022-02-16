534957810df960ca032cfc6fd0fd25b20cffe0b0
Junghun Park <junghun@MacBook-Pro.local>
2022-02-16 13:49:07 +0900

    Bounty List App

---
+++ BountyList/BountyViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by Junghun Park on 2022/02/16.
+//
+
+import Foundation
---
+++ BountyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BountyList
+//
+//  Created by Junghun Park on 2022/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
